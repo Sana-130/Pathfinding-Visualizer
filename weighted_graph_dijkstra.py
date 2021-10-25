72b9991d9790e5e734ab04ba1cd2a04ff80a02ca
Aysha sana <70009983+Sana-078@users.noreply.github.com>
2021-10-25 04:09:58 -0700

    Update weighted_graph_dijkstra.py

--- weighted_graph_dijkstra.py
+++ weighted_graph_dijkstra.py
@@ -1,8 +1,8 @@
 import pygame
 import math
-
 import random
 import time
+import sys
 
 
 ROW=25
@@ -17,24 +17,23 @@ done = False
 #colors
 BLACK = (0, 0, 0)
 WHITE=(255,255,255)
-#start and end node
+
 red=(222, 49, 99)
-purple=(106, 15, 142)
-#other nodes
-light_blue=(49, 172, 222)
+light_purple=(198, 178, 227)
+blue=(49, 172, 222)
 light_orange=(255, 154, 118)
-dark_blue=(51, 54, 255 )
+sky_blue=(160, 203, 222 )
+green=(17, 122, 101 )
 
-
+#other variables
 y0=0
 x1=20
 y1=20
-
 x0=0
-
 y=0
 n=20
 
+# Spot class
 class Spot:
     def __init__(self, x, y, col, row, width, distance):
         self.x=x
@@ -45,14 +44,14 @@ class Spot:
         self.color = WHITE
         self.distance=distance
         self.rect_obj=pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.width))
+        self.font=pygame.font.SysFont('Arial', 15)
         
 
     def draw_rect(self):
         pygame.draw.rect(surface, self.color, (self.x, self.y, self.width, self.width))
 
-    def draw_text(self):
-        
-        text_surface_object = pygame.font.SysFont('Arial', 15).render(str(self.distance), True, (255,0,0))
+    def draw_text(self): 
+        text_surface_object = self.font.render(str(self.distance), True, BLACK)
         text_rect = text_surface_object.get_rect(center=self.rect_obj.center)
         surface.blit(text_surface_object, text_rect)
 
@@ -60,21 +59,31 @@ class Spot:
         self.color=red
 
     def make_end(self):
-        self.color=purple
-
+        self.color=light_purple
+        
+    def make_barrier(self):
+        self.color=BLACK
         
     def visited_cell(self):
-        self.color=dark_blue
+        self.color=sky_blue
 
     def backtrack(self):
         self.color=light_orange
 
     def edge_color(self):
-        self.color=light_blue
+        self.color=blue
 
     def reset(self):
         self.color=WHITE
-
+        
+    # grass needs specific reset function as its distance value changes 
+    def grass(self):
+        self.color=green
+        self.distance=random.randint(10, 20)
+        
+    def grass_reset(self):
+        self.color=WHITE
+        self.distance=random.randint(1, 5)
 
 def grid_make(width):
     w=width+1
@@ -98,8 +107,10 @@ def draw(win, grid):
     win.fill(BLACK)
     for row in grid:
         for spot in row:
+            
             spot.draw_rect()
-            spot.draw_text()            
+            spot.draw_text()
+                        
     pygame.display.flip()
 
 '''this will get us the row and column position because
@@ -111,9 +122,9 @@ give the final value as the window size, because 13 is the default size after
 adding the spacing etc of the cell here.'''
 
 def get_position(x, y):
-    w=WIDTH+1
-    col=math.trunc(x/w)
-    row=math.trunc(y/w)
+    
+    col=math.trunc(x/26)
+    row=math.trunc(y/26)
     return col, row
 
 def construct_path(curr_node, from_list, start):
@@ -142,14 +153,10 @@ def dijkstra(draw, grid, start, end):
     
     #distance2={col: random.randint(1, 20) for row in grid for col in row }
     distance[start]=0
-    grid[0][0].distance=0
     visited_set=[]
-    
     from_list={}
-    
-
-    
-    def get_min_distance(grid, distance, visited):
+ 
+    def get_min_distance(distance, visited):
             
        try:
            min = sys.maxsize
@@ -166,8 +173,7 @@ def dijkstra(draw, grid, start, end):
     state=len(visited_set)!=total_cells
     while state:
         
-        current=get_min_distance(grid, distance, visited_set)
-        #print(current.distance, current.row, current.col)
+        current=get_min_distance(distance, visited_set)
         
         if current==False:
             state=False
@@ -192,45 +198,110 @@ def dijkstra(draw, grid, start, end):
                 if temp_dist <  distance[grid[row][col]]:
                     distance[grid[row][col]]=temp_dist
                     from_list[grid[row][col]]=current
-                    grid[row][col].edge_color()
-               
+                    if grid[row][col].color!=green:
+                        grid[row][col].edge_color()
+
+                #from_list[grid[row][col]]=current
                 #grid[row][col].edge_color()
             
             
-                time.sleep(0.001)
+                time.sleep(0.003)
                 draw()
                 
-        if current!=start:
+        if current!=start and current.color!=green:
             current.visited_cell()
-            
     
     return False
+
+#barrier reset
+def reset_barrier(grid):
+    for row in grid:
+        for col in row:
+            if col.color==BLACK:
+                col.reset()
+#grass reset
+def grass_reset(grid):
+    for row in grid:
+        for col in row:
+            if col.color==green:
+                col.grass_reset()
+
+#visualisation reset
+def visua_reset( grid, start, end):
+    if start!=None:
+        start.make_start()
+    if end!=None:
+        end.make_end()
+    for row in grid:
+        for col in row:
+            if col.color ==sky_blue or col.color==blue or col.color==light_orange:
+                col.reset()
+  
+                
 def main():
     run = True
-
-    
     grid=grid_make(WIDTH)
-
-    start=grid[0][0]
-    end=grid[5][5]
+    start=None
+    end=None
 
     while run:
-        draw(surface,grid)
+        draw(surface,grid )
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run=False
-                
+
+            #marking start node, end node and barreirs  
+            if pygame.mouse.get_pressed()[0]:
+                x, y=pygame.mouse.get_pos()
+                col, row=get_position(x,y)
+                node=grid[row][col]
+                if not start and node!=end:
+                    start=node
+                    start.make_start()
+               
                     
+                if not end and node!=start:
+                    end=node
+                    end.make_end()
+             
+
+                elif node!=start and node!=end:
+                    node.make_barrier()
+                    
+            # marking the grass cells
+            elif pygame.mouse.get_pressed()[2]:
+                x,y=pygame.mouse.get_pos()
+                col, row=get_position(x, y)
+                node=grid[row][col]
+                node.grass()
+                        
             if event.type==pygame.KEYDOWN:
+
+                #start and end node reset
+                if event.key==pygame.K_s and start and end:
+                    start.reset()
+                    end.reset()
+                    start=None
+                    end=None
+
+                #clearing out the barriers
+                if event.key==pygame.K_b :
+                    reset_barrier(grid)
+
+                #clearing out the grass cells
+                if event.key==pygame.K_g:
+                    grass_reset(grid)
                     
                 if event.key==pygame.K_d and start and end:
                     dijkstra(lambda: draw(surface, grid),grid, start, end)
+
+                #clearing out the visualisation part
+                if event.key==pygame.K_SPACE:
+                     visua_reset(grid, start, end)
                     
         
     pygame.quit()
-    
-
 
 
 if __name__=="__main__":
